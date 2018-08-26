55fdcd15d5500989821084f0c12570d0d1372bd9
Botmasher <languagejosh@hotmail.com>
2018-08-26 11:46:34 -0700

    Add notes and sketch of GameManager and Initializer

---
+++ Assets/_scripts/GameInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour {
+
+	public GameObject influenceBar;
+	public Transform influenceMarker;
+	public GameObject player;
+	private List<GameObject> players = new List<GameObject> ();
+	private int playerCount = 2;
+
+	void Start () {
+		for (int i=0; i < playerCount; i++) {
+			players.Add (Instantiate(player));
+		}
+		Instantiate (influenceBar);
+		Instantiate (influenceMarker);
+	}
+
+}
---
+++ Assets/_scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static float score = 0.0;
+	public float leak = 0.01; 			// rate of score loss over time
+
+	void Update () {
+		score = score - (score * leak * Time.deltaTime);
+	}
+}
